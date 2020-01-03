d66794d510817d980b0ec96ad7bf6d9acba872c9
Jherfson Castro <jherfson.castro@gmail.com>
2020-01-03 12:12:52 -0300

    separando a fase do potencial químico

--- PhaseDiagramOpen.py
+++ PhaseDiagramOpen.py
@@ -1,113 +1,163 @@
-"""
-This is a basic example of how to create, plot, and analyze OPEN Phase Diagrams using the pymatgen
-codebase and Materials Project database. To run this example, you should:
-* have pymatgen (www.pymatgen.org) installed along with matplotlib
-* obtain a Materials Project API key (https://www.materialsproject.org/open)
-* paste that API key in the MAPI_KEY variable below, e.g. MAPI_KEY = "foobar1234"
-For citation, see https://www.materialsproject.org/citing
-For the accompanying comic book, see http://www.hackingmaterials.com/pdcomic
-"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# from __future__ import print_function
+import settings
+from thermodynamics import Thermodynamics
 from pymatgen import MPRester, Element
-from pymatgen.analysis.phase_diagram import GrandPotentialPhaseDiagram, \
-    PhaseDiagram, PDPlotter
+from pymatgen.analysis.phase_diagram import GrandPotentialPhaseDiagram, PhaseDiagram, PDPlotter
+from pymatgen.ext.matproj import MPRester
+from pymatgen.apps.borg.hive import VaspToComputedEntryDrone
+from pymatgen.apps.borg.queen import BorgQueen
+from pymatgen.entries.compatibility import MaterialsProjectCompatibility
 
 
-#from graf.mu_to_T import print_T_corresponding_to_mu_equals
-# import mu_to_T
-from Thermodynamics import mu_to_temperature
-import config
+class PhaseDiagramOpenAnalyzer:
+    """
+    Phase diagram
+    """
 
-#imprimir = mu_to_temperature()
+    def __init__(self, system=[], open_element=""):
+        """
+        Phase diagram constructor
+        :param system: elements of system to analyze
+        :param open_element: element for potential change
+        """
+        self.system = system
+        self.open_element = open_element
 
+    @staticmethod
+    def plot_phase_diagram(pd, show_unstable=False):
+        """
+        Plot phase diagram for an specific composite
+        :param pd:
+        :param show_unstable:
+        """
+        plotter = PDPlotter(pd, show_unstable=show_unstable)
+        plotter.show()
 
-def plot_pd(pd, show_unstable=False):
-    plotter = PDPlotter(pd, show_unstable=show_unstable)
-    # plotter = PDPlotter(pd, show_unstable=0.03)
+    @staticmethod
+    def analyze_phase_diagram(phase_diagram):
+        """
+        Analyze phase diagram for an specific composite
+        :param phase_diagram:
+        """
+        for e in phase_diagram.stable_entries:
+            print(e.composition.reduced_formula, e.entry_id)
 
-    plotter.show()
-    # plotter.write_image("{}.png".format('-'.join(system)), "png")  # save figure
+        for e in phase_diagram.unstable_entries:
+            decomp, e_above_hull = phase_diagram.get_decomp_and_e_above_hull(e)
+            pretty_decomp = [("{}:{}".format(k.composition.reduced_formula, k.entry_id), round(v, 2)) for k, v in
+                             decomp.items()]
+            print(e.composition.reduced_formula, e.entry_id, "%.3f" %
+                  e_above_hull, pretty_decomp)
 
+    def get_grand_potential_phase_diagram(self, gcpdobj=None):
+        """
+        Returns grand potential phase diagram
+        :param gcpdobj:
+        :return: All phases with their own potential
+        """
+        if gcpdobj is None:
+            return []
 
-def analyze_pd(pd):
-    print('Stable Entries (formula, materials_id)\n--------')
-    for e in pd.stable_entries:
-        print(e.composition.reduced_formula, e.entry_id)
+        potential = [v for el, v in gcpdobj.chempots.items()]
+        phases = [entry.name for entry in gcpdobj.stable_entries]
+        # return phases, potential[0]
+        return phases, potential[0]
 
-    print('\nUnstable Entries (formula, materials_id, e_above_hull (eV/atom), decomposes_to)\n--------')
-    for e in pd.unstable_entries:
-        decomp, e_above_hull = pd.get_decomp_and_e_above_hull(e)
-        pretty_decomp = [("{}:{}".format(k.composition.reduced_formula, k.entry_id), round(v, 2)) for k, v in
-                         decomp.items()]
-        print(e.composition.reduced_formula, e.entry_id, "%.3f" % e_above_hull, pretty_decomp)
+    def get_phase_diagram_data(self):
+        """
+        Returns grand potential phase diagram data to external plot
+        Assumes openelement specific element equals None
+        :return: Data to external plot
+        """
+        open_elements_specific = None
+        open_element_all = Element(self.open_element)
+        mpr = MPRester(settings.apiKey)
 
+        drone = VaspToComputedEntryDrone()
+        queen = BorgQueen(drone, rootpath=".")
+        entries = queen.get_data()
 
-def get_gcpd_data(gcpdobj=None):
-    if gcpdobj is None:
-        return []
+        # Get data to make phase diagram
+        mp_entries = mpr.get_entries_in_chemsys(
+            self.system, compatible_only=True)
 
-    #  potential = [v for el, v in gcpdobj.chempots.items()]
-    phases = ", ".join([entry.name for entry in gcpdobj.stable_entries])
+        entries.extend(mp_entries)
 
-    return [phases]
+        compat = MaterialsProjectCompatibility()
+        entries = compat.process_entries(entries)
+        #explanation_output = open("explain.txt",'w')
+        #entries_output = open("entries.txt", 'w')
+        compat.explain(entries[0])
+        #print(entries, file=entries_output)
 
+        if open_elements_specific:
+            gcpd = GrandPotentialPhaseDiagram(entries, open_elements_specific)
+            self.plot_phase_diagram(gcpd, False)
+            self.analyze_phase_diagram(gcpd)
 
-if __name__ == "__main__":
-    MAPI_KEY = config.KEY["key"] # You must change this to your Materials API key! (or set MAPI_KEY env variable)
-    system = ["Li", "B", "O"]  # system we want to get open PD for
-    # system = ["Li", "Fe", "P", "O"]  # alternate system example
+        if open_element_all:
+            pd = PhaseDiagram(entries)
+            chempots = pd.get_transition_chempots(open_element_all)
+            # print(chempots)
+            #all_gcpds = list()
+            toplot = []
+            # dic = {}
+            for idx in range(len(chempots)):
+                if idx == len(chempots) - 1:
+                    avgchempot = chempots[idx] - 0.1
+                else:
+                    avgchempot = 0.5 * (chempots[idx] + chempots[idx + 1])
+                gcpd = GrandPotentialPhaseDiagram(
+                    entries, {open_element_all: avgchempot}, pd.elements)
 
-    open_elements_specific = None  # e.g., {Element("O"): 0} where 0 is the specific chemical potential
-    open_element_all = Element("O")  # plot a series of open phase diagrams at critical chem pots with this element open
+                # min_chempot = None if idx == len(
+                #     chempots) - 1 else chempots[idx + 1]
+                # max_chempot = chempots[idx]
+                #gcpd = GrandPotentialPhaseDiagram(entries, {open_element_all: max_chempot}, pd.elements)
 
-    mpr = MPRester(MAPI_KEY)  # object for connecting to MP Rest interface
+                toplot.append(self.get_grand_potential_phase_diagram(gcpd))
+                # toplot.append(max_chempot)
 
-    # get data
-    entries = mpr.get_entries_in_chemsys(system, compatible_only=True)
+                #self.plot_phase_diagram(gcpd, False)
+                #print({open_element_all: max_chempot})
 
-    if open_elements_specific:
-        gcpd = GrandPotentialPhaseDiagram(entries, open_elements_specific)
-        plot_pd(gcpd, False)
-        analyze_pd(gcpd)
+        all_phase_diagrams = toplot
+        # print(all_phase_diagrams)
 
-    if open_element_all:
-        pd = PhaseDiagram(entries)
-        chempots = pd.get_transition_chempots(open_element_all)
-        all_gcpds = list()
-        toplot = []
-        arquivo = open("dados.txt", "w")
-        for idx in range(len(chempots)):
-            if idx == len(chempots) - 1:
-                avgchempot = chempots[idx] - 0.1
+        number_of_phase_diagrams = len(all_phase_diagrams)
+
+        #pd3 = PhaseDiagram(entries)
+
+        chempot_list = pd.get_transition_chempots(open_element_all)
+        pd_index = 0
+
+        chempots_range_of_each_phase = {}
+        for particular_phase_diagram in all_phase_diagrams:
+            chempot = chempot_list[pd_index]
+
+            if pd_index is not number_of_phase_diagrams - 1:
+                next_chempot = chempot_list[pd_index + 1]
             else:
-                avgchempot = 0.5 * (chempots[idx] + chempots[idx + 1])
-            gcpd = GrandPotentialPhaseDiagram(entries, {open_element_all: avgchempot}, pd.elements)
+                next_chempot = chempot_list[pd_index] - 2.0
+            chempot_range = [chempot, next_chempot]
 
-            min_chempot = None if idx == len(chempots) - 1 else chempots[idx + 1]
-            max_chempot = chempots[idx]
-            #  print("Chempot range for diagram {} is: {} to {}".format(idx, min_chempot, max_chempot))
+            phases_list = particular_phase_diagram[0]
 
-            toplot.append(get_gcpd_data(gcpd))
+            for phase in phases_list:
+                if phase in chempots_range_of_each_phase.keys():
+                    chempots_range_of_each_phase[phase][1] = next_chempot.copy(
+                    )
+                else:
+                    chempots_range_of_each_phase[phase] = chempot_range.copy()
 
-           
-           
-            plot_pd(gcpd, False)
+            pd_index = pd_index + 1
 
-
-
-
-
-
+        return chempots_range_of_each_phase
+        # print(chempots_range_of_each_phase)
 
 
-
-
-
-
-
-
-
-
-
-
+# if __name__ == "__main__":
+#     dado = PhaseDiagramOpenAnalyzer(system=["Li", "Ca", "O"], open_element="O")
+#     dado.get_phase_diagram_data()
